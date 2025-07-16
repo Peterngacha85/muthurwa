3b9a4bfbee571ad274eca07183824672a615013c
Peter Ngacha <peterngacha85@gmail.com>
2025-07-16 20:46:28 +0300

    origin

--- backend/server.js
+++ backend/server.js
@@ -14,8 +14,8 @@ app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
 app.use(express.json());
 app.use(cors({
   origin: [
-    'https://muthurwa.fastweb.co.ke',   // Any custom domain you use
-    // 'https://muthurwa.vercel.app',      // Your Vercel frontend
+    'https://market.fastweb.co.ke',   // Any custom domain you use
+    'https://muthurwa.vercel.app',      // Your Vercel frontend
   ],
   credentials: true
 }));
